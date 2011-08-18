ee0a63ba39e6f8fb7720bcdc290dc2df346691f8
alelody <alelody@gmail.com>
2011-08-19 02:14:35 +0800

    3 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiggShovell::Application.config.secret_token = 'e053071452f36b4dbd6f68202c05f35524c86e3018a0a77fcf0169d5594b4195dee7594c6404f9e3540f377a224435d807fe5427598d744ebd64aa49edb795fe'
